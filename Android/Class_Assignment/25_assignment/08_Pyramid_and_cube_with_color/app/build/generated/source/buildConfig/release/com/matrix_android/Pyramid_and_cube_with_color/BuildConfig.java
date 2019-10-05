6ff16da3663cc9c683641004fcc9fa90a1fd5510
Bhushan Kalamkar <bhushan3130@gmail.com>
2019-10-06 01:46:31 +0530

    Android Assignment

---
+++ Android/Class_Assignment/25_assignment/08_Pyramid_and_cube_with_color/app/build/generated/source/buildConfig/release/com/matrix_android/Pyramid_and_cube_with_color/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.matrix_android.Pyramid_and_cube_with_color;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.matrix_android.Pyramid_and_cube_with_color";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
