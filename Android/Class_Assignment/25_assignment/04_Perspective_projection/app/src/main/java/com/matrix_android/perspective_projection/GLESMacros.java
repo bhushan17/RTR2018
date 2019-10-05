5066408540406edf45a2bcde022e464b565ea694
Bhushan Kalamkar <bhushan3130@gmail.com>
2019-10-05 18:52:55 +0530

    Android assignment

---
+++ Android/Class_Assignment/25_assignment/04_Perspective_projection/app/src/main/java/com/matrix_android/perspective_projection/GLESMacros.java
@@ -0,0 +1,8 @@
+package com.matrix_android.perspective_projection;
+
+public class GLESMacros{
+	public static final int AMC_ATTRIBUTE_POSITION = 0;
+	public static final int AMC_ATTRIBUTE_COLOR = 1 ;
+	public static final int AMC_ATTRIBUTE_NORMAL = 2;
+	public static final int AMC_ATTRIBUTE_TEXCOORD0 = 3;
+}
