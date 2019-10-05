5bc47cff7479590ddf719e5eb0b35e68a8a2a3a6
Bhushan Kalamkar <bhushan3130@gmail.com>
2019-10-06 01:51:14 +0530

    Android Assignment

---
+++ Android/Class_Assignment/25_assignment/15_AllGeometry_on_graphPaper/app/src/main/java/com/matrix_android/AllGeometry_on_graphPaper/GLESMacros.java
@@ -0,0 +1,8 @@
+package com.matrix_android.AllGeometry_on_graphPaper;
+
+public class GLESMacros{
+	public static final int AMC_ATTRIBUTE_POSITION = 0;
+	public static final int AMC_ATTRIBUTE_COLOR = 1 ;
+	public static final int AMC_ATTRIBUTE_NORMAL = 2;
+	public static final int AMC_ATTRIBUTE_TEXCOORD0 = 3;
+}
