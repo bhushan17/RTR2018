c6f25992bbe918c034259a4375ac281e928b6f56
pournimap <pournimap28@gmail.com>
2020-01-17 14:38:53 +0530

    DirectX

---
+++ DirectX/Direct3D/01_BlueWindowDirectX/D3D_Window.cpp
@@ -0,0 +1,501 @@
+
+#include<windows.h>
+#include<stdio.h> // for file I/O
+
+#include<d3d11.h>
+
+#pragma comment(lib, "d3d11.lib")
+
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+//global function declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//global variable declarartion
+
+FILE *gpFile = NULL;
+char gszLogFileName[] = "Log.txt";
+
+HWND ghwnd = NULL;
+
+DWORD dwStyle;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+
+bool gbActiveWindow = false;
+bool gbEscapeKeyIsPressed = false;
+bool gbFullscreen = false;
+
+float gClearColor[4]; // RGBA
+IDXGISwapChain *gpIDXGISwapChain = NULL;
+ID3D11Device *gpID3D11Device = NULL;
+ID3D11DeviceContext *gpID3D11DeviceContext = NULL;
+ID3D11RenderTargetView *gpID3D11RenderTargetView = NULL;
+
+//WinMain
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//function declaration
+	HRESULT initialize(void);
+	void unintialize(void);
+	void display(void);
+
+	//variable declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szClassName[] = TEXT("Direct3D11");
+	bool bDone = false;
+
+	//code
+	// create log file
+	if (fopen_s(&gpFile, gszLogFileName, "w") != 0)
+	{
+		MessageBox(NULL, TEXT("Log File Can Not be Ceated \n Exitting ..."), TEXT("ERROR"), MB_OK | MB_TOPMOST | MB_ICONSTOP);
+		exit(0);
+	}
+	else
+	{
+		fprintf_s(gpFile, "Log File Is Successfully Opened. \n");
+		fclose(gpFile);
+	}
+
+	//intialize WNDCLASSEX structure
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.lpszClassName = szClassName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//register WNDCLASSEX structure
+	RegisterClassEx(&wndclass);
+
+	//create window
+	hwnd = CreateWindow(szClassName,
+		TEXT("Direct3D11 Bhushan's Window "),
+		WS_OVERLAPPEDWINDOW,
+		100,
+		100,
+		WIN_WIDTH,
+		WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ghwnd = hwnd;
+
+	ShowWindow(hwnd, iCmdShow);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	//intialize D3D
+	HRESULT hr = S_OK;
+	hr = initialize();
+
+	if (FAILED(hr))
+	{
+		fopen_s(&gpFile, gszLogFileName, "a+");
+		fprintf_s(gpFile, "intialize() Failed. Exitting Now ... \n");
+		fclose(gpFile);
+		DestroyWindow(hwnd);
+		hwnd = NULL;
+	}
+	else
+	{
+		fopen_s(&gpFile, gszLogFileName, "a+");
+		fprintf_s(gpFile, "intialize() Succeeded.\n");
+		fclose(gpFile);
+	}
+
+	//message loop
+	while (bDone == false)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				bDone = true;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			//render
+			display();
+			if (gbActiveWindow == true)
+			{
+				if (gbEscapeKeyIsPressed == true)
+					bDone = true;
+			}
+		}
+	}
+
+	//cleanUp
+	unintialize();
+
+	return((int)msg.wParam);
+
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//function declarations
+	HRESULT resize(int, int);
+	void ToggleFullscreen(void);
+	void unintialize(void);
+
+	//variable declarartion
+	HRESULT hr;
+
+	//code
+	switch (iMsg)
+	{
+	case WM_ACTIVATE:
+		if (HIWORD(wParam) == 0) //if 0, window is active
+			gbActiveWindow = true;
+		else  //if non-zero , window is not active
+			gbActiveWindow = false;
+		break;
+	case WM_ERASEBKGND:
+		return(0);
+	case WM_SIZE:
+		if (gpID3D11DeviceContext)
+		{
+			hr = resize(LOWORD(lParam), HIWORD(lParam));
+			if (FAILED(hr))
+			{
+				fopen_s(&gpFile, gszLogFileName, "a+");
+				fprintf_s(gpFile, "resize() Failed. \n");
+				fclose(gpFile);
+				return(hr);
+			}
+			else
+			{
+				fopen_s(&gpFile, gszLogFileName, "a+");
+				fprintf_s(gpFile, "resize() Succeeded. \n");
+				fclose(gpFile);
+			}
+		}
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE: // case 27
+			if (gbEscapeKeyIsPressed == false)
+				gbEscapeKeyIsPressed = true;
+			break;
+		case 0x46 : //F 0r f
+			if (gbFullscreen == false)
+			{
+				ToggleFullscreen();
+				gbFullscreen = true;
+			}
+			else
+			{
+				ToggleFullscreen();
+				gbFullscreen = false;
+			}
+			break;
+		default : 
+			break;
+
+		}
+		break;
+	case WM_NCLBUTTONDOWN:
+		break;
+	case WM_CLOSE:
+		unintialize();
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default :
+		break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullscreen(void)
+{
+	MONITORINFO mI;
+	if (gbFullscreen == false)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		mI = { sizeof(MONITORINFO) };
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mI))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd,
+					HWND_TOP,
+					mI.rcMonitor.left,
+					mI.rcMonitor.top,
+					mI.rcMonitor.right - mI.rcMonitor.left,
+					mI.rcMonitor.bottom - mI.rcMonitor.top,
+					SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+		}
+		ShowCursor(FALSE);
+		gbFullscreen = true;
+	}
+	else
+	{
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd,
+			HWND_TOP,
+			0,
+			0,
+			0,
+			0,
+			SWP_NOZORDER | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER);
+		ShowCursor(TRUE);
+		gbFullscreen = false;
+	}
+}
+
+HRESULT initialize(void)
+{
+	//function declarations
+	void uninitialize(void);
+	HRESULT resize(int, int);
+
+	//variable declarations
+	HRESULT hr = S_OK;
+	D3D_DRIVER_TYPE d3dDriverType;
+	D3D_DRIVER_TYPE d3dDriverTypes[]={ D3D_DRIVER_TYPE_HARDWARE, D3D_DRIVER_TYPE_WARP, D3D_DRIVER_TYPE_REFERENCE};
+
+	D3D_FEATURE_LEVEL d3dFeatureLevel_required = D3D_FEATURE_LEVEL_11_0;
+	D3D_FEATURE_LEVEL d3dFeatureLevel_acquired = D3D_FEATURE_LEVEL_10_0; //default , lowest
+
+	UINT createDeviceFlags = 0;
+	UINT numDriverTypes = 0;
+	UINT numFeatureLevels = 1; //based upon d3dFeatureLevel_required
+
+	//code
+	numDriverTypes = sizeof(d3dDriverTypes) / sizeof(d3dDriverTypes[0]); // calculation size of array
+
+	DXGI_SWAP_CHAIN_DESC dxgiSwapChainDesc;
+	ZeroMemory((void*)&dxgiSwapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+	dxgiSwapChainDesc.BufferCount = 1;
+	dxgiSwapChainDesc.BufferDesc.Width = WIN_WIDTH;
+	dxgiSwapChainDesc.BufferDesc.Height = WIN_HEIGHT;
+	dxgiSwapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	dxgiSwapChainDesc.BufferDesc.RefreshRate.Numerator = 60;
+	dxgiSwapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+	dxgiSwapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	dxgiSwapChainDesc.OutputWindow = ghwnd;
+	dxgiSwapChainDesc.SampleDesc.Count = 1;
+	dxgiSwapChainDesc.SampleDesc.Quality = 0;
+	dxgiSwapChainDesc.Windowed = TRUE;
+
+	for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
+	{
+		d3dDriverType = d3dDriverTypes[driverTypeIndex];
+		hr = D3D11CreateDeviceAndSwapChain(
+			NULL,  //Adapter
+			d3dDriverType, //Driver Type
+			NULL,  // SoftWare
+			createDeviceFlags, //Flags
+			&d3dFeatureLevel_required,  //Feature Levels
+			numFeatureLevels,  //Num Feaure Levels
+			D3D11_SDK_VERSION,  // SDK_VERSION
+			&dxgiSwapChainDesc,  // Swap chain Desc
+			&gpIDXGISwapChain,   //swap chain
+			&gpID3D11Device,  // Device
+			&d3dFeatureLevel_acquired,  // Feature Level
+			&gpID3D11DeviceContext);   // Device Context
+
+		if (SUCCEEDED(hr))
+		{
+			break;
+		}
+
+		
+	}
+
+	if (FAILED(hr))
+	{
+		fopen_s(&gpFile, gszLogFileName, "a+");
+		fprintf_s(gpFile, "D3D11CreateDeviceAndSwapChain() Failed. \n");
+		fclose(gpFile);
+		return(hr);
+	}
+	else
+	{
+		fopen_s(&gpFile, gszLogFileName, "a+");
+		fprintf_s(gpFile, "D3D11CreateDeviceAndSwapChain() Succeeded. \n");
+		fprintf_s(gpFile, "The Chosen Driver Is of. \n");
+		if (d3dDriverType == D3D_DRIVER_TYPE_HARDWARE)
+		{
+			fprintf_s(gpFile, "HArdware Type. \n");
+		}
+		else if (d3dDriverType == D3D_DRIVER_TYPE_WARP)
+		{
+			fprintf_s(gpFile, "WARP Type. \n");
+		}
+		else if (d3dDriverType == D3D_DRIVER_TYPE_REFERENCE)
+		{
+			fprintf_s(gpFile, "Reference Type. \n");
+		}
+		else
+		{
+			fprintf_s(gpFile, "Unknow Type. \n");
+		}
+
+		fprintf_s(gpFile, "The Supported Heighest Feature Level Is");
+		if (d3dFeatureLevel_acquired == D3D_FEATURE_LEVEL_11_0)
+		{
+			fprintf_s(gpFile, "11.0\n");
+		}
+		else if (d3dFeatureLevel_acquired == D3D_FEATURE_LEVEL_10_1)
+		{
+			fprintf_s(gpFile, "10.1\n");
+		}
+		else if (d3dFeatureLevel_acquired == D3D_FEATURE_LEVEL_10_0)
+		{
+			fprintf_s(gpFile, "10.0\n");
+		}
+		else
+		{
+			fprintf_s(gpFile, "Unknown.\n");
+		}
+
+		fclose(gpFile);
+	}
+
+
+	gClearColor[0] = 0.0f;
+	gClearColor[1] = 0.0f;
+	gClearColor[2] = 1.0f;
+	gClearColor[3] = 1.0f;
+
+	//call resize for first time
+	hr = resize(WIN_WIDTH, WIN_HEIGHT);
+	if (FAILED(hr))
+	{
+		fopen_s(&gpFile, gszLogFileName, "a+");
+		fprintf_s(gpFile, "resize() Failed. \n");
+		fclose(gpFile);
+		return(hr);
+	}
+	else
+	{
+		fopen_s(&gpFile, gszLogFileName, "a+");
+		fprintf_s(gpFile, "resize() succeeded. \n");
+		fclose(gpFile);
+		
+	}
+
+	return(S_OK);
+}
+
+HRESULT resize(int width, int height)
+{
+	//code
+	HRESULT hr = S_OK;
+
+	//free any size-dependant resources
+	if (gpID3D11RenderTargetView)
+	{
+		gpID3D11RenderTargetView->Release();
+		gpID3D11RenderTargetView = NULL;
+	}
+
+	//resize swap chain buffers accordingly
+	gpIDXGISwapChain->ResizeBuffers(1, width, height, DXGI_FORMAT_R8G8B8A8_UNORM, 0);
+
+	//again get back buffer from swap chain
+	ID3D11Texture2D *pID3D11Texture2D_BackBuffer;
+	gpIDXGISwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pID3D11Texture2D_BackBuffer);
+
+	//again get render target view from d3d11 device using above back buffer
+	hr = gpID3D11Device->CreateRenderTargetView(pID3D11Texture2D_BackBuffer, NULL, &gpID3D11RenderTargetView);
+
+	if (FAILED(hr))
+	{
+		fopen_s(&gpFile, gszLogFileName, "a+");
+		fprintf_s(gpFile, "ID3D11Device :: CreateRenderTargetView()  Failed. \n");
+		fclose(gpFile);
+		return(hr);
+	}
+	else
+	{
+		fopen_s(&gpFile, gszLogFileName, "a+");
+		fprintf_s(gpFile, "ID3D11Device :: CreateRenderTargetView()  Succeeded. \n");
+		fclose(gpFile);
+	}
+
+	pID3D11Texture2D_BackBuffer->Release();
+	pID3D11Texture2D_BackBuffer = NULL;
+
+	//Set ViewPort
+	D3D11_VIEWPORT d3dViewPort;
+	d3dViewPort.TopLeftX = 0;
+	d3dViewPort.TopLeftY = 0;
+	d3dViewPort.Width = (float)width;
+	d3dViewPort.Height = (float)height;
+	d3dViewPort.MinDepth = 0.0f;
+	d3dViewPort.MaxDepth = 1.0f;
+	gpID3D11DeviceContext->RSSetViewports(1, &d3dViewPort);
+
+	return(hr);
+}
+
+void display(void)
+{
+	//code
+	//clear render target view to a chosen color
+	gpID3D11DeviceContext->ClearRenderTargetView(gpID3D11RenderTargetView, gClearColor);
+	
+	//switch between front and back buffer
+	gpIDXGISwapChain->Present(0, 0);
+}
+
+void unintialize(void)
+{
+	//code
+	if (gpID3D11RenderTargetView)
+	{
+		gpID3D11RenderTargetView->Release();
+		gpID3D11RenderTargetView = NULL;
+	}
+
+	if (gpIDXGISwapChain)
+	{
+		gpIDXGISwapChain->Release();
+		gpIDXGISwapChain = NULL;
+	}
+
+	if (gpID3D11DeviceContext)
+	{
+		gpID3D11DeviceContext->Release();
+		gpID3D11DeviceContext = NULL;
+	}
+
+	if (gpID3D11Device)
+	{
+		gpID3D11Device->Release();
+		gpID3D11Device = NULL;
+	}
+
+	if (gpFile)
+	{
+		fopen_s(&gpFile, gszLogFileName, "a+");
+		fprintf_s(gpFile, "unintialize() Succeeded\n");
+		fprintf_s(gpFile, "Log File Is Successfully Closed.\n");
+		fclose(gpFile);
+	}
+}
